d2f2b9bf48f1eef9a3d86e420202be8d5aea9b44
Guy John <guy@rumblesan.com>
2017-08-10 22:11:00 +0100

    Add further bytecode types

--- src/vm/bytecode.js
+++ src/vm/bytecode.js
@@ -11,8 +11,20 @@ export const LSS = 'LSS'; // Pop top two values, push 1 if first is less than se
 
 export const JMP = 'JMP'; // Jump to a new position
 export const BCH = 'BCH'; // Pop a value off the stack and jump if it's not zero
+
+// Stack and Heap manipulation
 export const PSH = 'PSH'; // Push a value onto the stack
 export const POP = 'POP'; // Pop a value off the stack
+export const LOD = 'LOD'; // Load a value from the heap
+export const SAV = 'SAV'; // Save a value to the heap
+
+// List Ops
+export const LST = 'LST'; // Create a list object
+export const LEN = 'LEN'; // Get the length of the list
+export const IDX = 'IDX'; // Get a value from a given index in the list
+export const SDX = 'SDX'; // Save a value to a given index in the list
+export const LPO = 'LPO'; // Pop a value from a list
+export const LPU = 'LPU'; // Push a value to a list
 
 // Misc
 export const NOP = 'NOP'; // No Operation
