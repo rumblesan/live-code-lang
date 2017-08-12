24633ca83e1157c9f10b527e631b4ba7bc789995
Guy John <guy@rumblesan.com>
2017-08-12 08:17:20 +0100

    Move lamda lifter into its own file

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,90 +1,44 @@
 import {
-  // Null,
+  Null,
   // Block,
-  Assignment,
+  // Assignment,
   // Application,
   // If,
-  Lambda,
+  // Lambda,
   // Times,
   // DoOnce,
   // UnaryOp,
   // BinaryOp,
   // DeIndex,
   // Num,
-  Variable,
+  // Variable,
   // Str,
   // List,
 } from 'ast';
 
 import {
-  // NULL,
+  NULL,
   // BLOCK,
-  ASSIGNMENT,
+  // ASSIGNMENT,
   // APPLICATION,
   // IF,
-  LAMBDA,
+  // LAMBDA,
   // TIMES,
   // DOONCE,
   // UNARYOP,
   // BINARYOP,
   // DEINDEX,
   // NUMBER,
-  VARIABLE,
+  // VARIABLE,
   // STRING,
   // LIST,
 } from 'ast/types';
 
+import { lambdaLifter } from './lambdaLifter';
+
 import { astTransform } from 'ast/func';
 
-export function variableLifter(
-  ast,
-  initialState = {
-    parentScope: {},
-    scope: {},
-    free: [],
-  }
-) {
-  return astTransform(
-    ast,
-    {
-      [VARIABLE]: (variable, transFuncs, state) => {
-        if (!state.scope[variable.identifier]) {
-          state.free.push(variable.identifier);
-        }
-        return Variable(variable.identifier);
-      },
-
-      [ASSIGNMENT]: (assignment, transFuncs, state) => {
-        const expr = variableLifter(assignment.expression, state);
-        if (state.parentScope[assignment.identifier]) {
-          state.free.push(assignment.identifier);
-        } else {
-          state.scope[assignment.identifier] = true;
-        }
-        return Assignment(assignment.identifier, expr);
-      },
-
-      [LAMBDA]: (lambda, transFuncs, state) => {
-        const newParent = Object.create(state.parentScope);
-        Object.assign(newParent, state.scope);
-        const newScope = lambda.argNames.reduce((s, name) => {
-          s[name] = true;
-          return s;
-        }, {});
-
-        const newState = { scope: newScope, free: [], parentScope: newParent };
-        const newBody = variableLifter(lambda.body, newState);
-        const freeVars = newState.free;
-
-        freeVars.forEach(v => {
-          if (!state.scope[v]) {
-            state.free.push(v);
-          }
-        });
-
-        return Lambda(lambda.argNames, newBody, lambda.inlinable, freeVars);
-      },
-    },
-    initialState
-  );
+export function compile(ast) {
+  const lifted = lambdaLifter(ast);
+  return astTransform(lifted, {});
 }
